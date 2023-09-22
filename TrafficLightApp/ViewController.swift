bec2c3526a103b353af3fe31279506565f48cf0e
Wh1teSparrow <wwhitesparroww@gmail.com>
2023-09-22 22:25:49 +0500

    WorkInProgress

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -11,10 +11,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var lightButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
+        lightButton.layer.cornerRadius = 10
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
     }
 }
 
