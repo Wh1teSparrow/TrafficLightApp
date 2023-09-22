b84c024dd121373992076cf1d31554785e204744
Wh1teSparrow <wwhitesparroww@gmail.com>
2023-09-22 18:06:57 +0500

    1. Set button 2. Set three view

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 75
     }
-
-
 }
 
