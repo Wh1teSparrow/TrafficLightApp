797df572ce516909497263a76fdf2d35894166fc
Wh1teSparrow <wwhitesparroww@gmail.com>
2023-09-25 01:24:38 +0500

    1. Work with logic 2. Work with View 3. Set app icon 4. Work with LaunchScreen

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -8,38 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var redLight: UIView!
-    @IBOutlet weak var yellowLight: UIView!
-    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
     @IBOutlet weak var lightButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = 75
-        yellowLight.layer.cornerRadius = 75
-        greenLight.layer.cornerRadius = 75
+        redLightView.layer.cornerRadius = 75
+        yellowLightView.layer.cornerRadius = 75
+        greenLightView.layer.cornerRadius = 75
         lightButton.layer.cornerRadius = 30
-        redLight.alpha = 0.3
-        yellowLight.alpha = 0.3
-        greenLight.alpha = 0.3
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
     
     @IBAction func lightButtonPressed() {
-        if redLight.alpha != 1 {
-            redLight.alpha = 1
+        if redLightView.alpha < 0.4 {
+            redLightView.alpha = 1
             lightButton.setTitle("NEXT", for: .normal)
-        } else if yellowLight.alpha != 1 {
-            redLight.alpha = 0.3
-            yellowLight.alpha = 1
-        } else if greenLight.alpha != 1 {
-            greenLight.alpha = 1
+        } else if yellowLightView.alpha < 0.4 {
+            redLightView.alpha = 0.4
+            yellowLightView.alpha = 1
+        } else if greenLightView.alpha < 0.4 {
+            yellowLightView.alpha = 0.4
+            greenLightView.alpha = 1
             lightButton.setTitle("OFF", for: .normal)
         } else {
-            redLight.alpha = 0.3
-            yellowLight.alpha = 0.3
-            greenLight.alpha = 0.3
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
             lightButton.setTitle("START", for: .normal)
         }
     }
 }
-
